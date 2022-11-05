5f4570d2fae97c7d4f4f409a86c7b01f1ee744ad
SilvaAvelinoAndre <andreavelinosilva@hotmail.com>
2022-11-05 14:13:46 -0300

    Criando model e tabela no banco

--- CRUDspringboot/src/main/java/br/com/springboot/CRUDspringboot/controllers/GreetingsController.java
+++ CRUDspringboot/src/main/java/br/com/springboot/CRUDspringboot/controllers/GreetingsController.java
@@ -13,14 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class GreetingsController {
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     */
-    @RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String greetingText(@PathVariable String name) {
-        return "Curso SpringBoot API" + name + "!";
-    }
+	/**
+	 *
+	 * @param name the name to greet
+	 * @return greeting text
+	 */
+	@RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public String greetingText(@PathVariable String name) {
+		return "Curso SpringBoot API" + name + "!";
+	}
+	@RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public String retornaOlaMundo(@PathVariable String nome) {
+		return "Olá Mundo " + nome + "!";
+	}
+
 }
